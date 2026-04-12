8f8a5fd53ae27d8e40f3cf08721f7f123902d357
parkdoyoung <qkrehdud11@gmail.com>
2026-04-12 23:40:59 +0900

    add

---
+++ Balacing_Robot.srcs/sources_1/new/l298n.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/12/2026 11:40:38 PM
+// Design Name: 
+// Module Name: l298n
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module l298n(
+
+    );
+endmodule
