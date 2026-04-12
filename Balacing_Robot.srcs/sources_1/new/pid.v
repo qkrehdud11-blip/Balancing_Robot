9562895bb85b61a15b919930b44c2a175b399b10
Appletea <dongs4739@gmail.com>
2026-04-13 01:33:16 +0900

    Add claude.md on gitignore Add pid.v

---
+++ Balacing_Robot.srcs/sources_1/new/pid.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/13/2026 01:29:18 AM
+// Design Name: 
+// Module Name: pid
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pid(
+
+    );
+endmodule
